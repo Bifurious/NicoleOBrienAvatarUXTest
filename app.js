f96d4818834897fcb5548134ca45c7113999b750
Bifurious <nicoleobrien304@hotmail.co.uk>
2023-10-19 23:29:39 +0100

    importing images and fiddled around with code

--- app.js
+++ app.js
@@ -1,29 +1,103 @@
 const Application = PIXI.Application;
 
-const app = new PIXI.Application({
-    background: '#1099bb',
-    resizeTo: window,
-});
-
-document.body.appendChild(app.view);
-
-// create a new Sprite from an image path
-const bunny = PIXI.Sprite.from('https://pixijs.com/assets/bunny.png');
-
-// center the sprite's anchor point
-bunny.anchor.set(0.5);
-
-// move the sprite to the center of the screen
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
-
-app.stage.addChild(bunny);
-
-// Listen for animate update
-app.ticker.add((delta) =>
-{
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent transformation
-    bunny.rotation += 0.1 * delta;
-});
+(function () {
+    "use strict";
+  
+    const items = [
+      "7️⃣",
+      "❌",
+      "🍓",
+      "🍋",
+      "🍉",
+      "🍒",
+      "💵",
+      "🍊",
+      "🍎"
+    ];
+    console.log(items);
+    document.querySelector(".info").textContent = items.join(" ");
+  
+    const doors = document.querySelectorAll(".door");
+    document.querySelector("#spinner").addEventListener("click", spin);
+    document.querySelector("#reseter").addEventListener("click", init);
+  
+    async function spin() {
+      init(false, 1, 2);
+      for (const door of doors) {
+        const boxes = door.querySelector(".boxes");
+        const duration = parseInt(boxes.style.transitionDuration);
+        boxes.style.transform = "translateY(0)";
+        await new Promise((resolve) => setTimeout(resolve, duration * 100));
+      }
+    }
+  
+    function init(firstInit = true, groups = 1, duration = 1) {
+      for (const door of doors) {
+        if (firstInit) {
+          door.dataset.spinned = "0";
+        } else if (door.dataset.spinned === "1") {
+          return;
+        }
+  
+        const boxes = door.querySelector(".boxes");
+        const boxesClone = boxes.cloneNode(false);
+  
+        const pool = ["❓"];
+        if (!firstInit) {
+          const arr = [];
+          for (let n = 0; n < (groups > 0 ? groups : 1); n++) {
+            arr.push(...items);
+          }
+          pool.push(...shuffle(arr));
+  
+          boxesClone.addEventListener(
+            "transitionstart",
+            function () {
+              door.dataset.spinned = "1";
+              this.querySelectorAll(".box").forEach((box) => {
+                box.style.filter = "blur(1px)";
+              });
+            },
+            { once: true }
+          );
+  
+          boxesClone.addEventListener(
+            "transitionend",
+            function () {
+              this.querySelectorAll(".box").forEach((box, index) => {
+                box.style.filter = "blur(0)";
+                if (index > 0) this.removeChild(box);
+              });
+            },
+            { once: true }
+          );
+        }
+  
+        for (let i = pool.length - 1; i >= 0; i--) {
+          const box = document.createElement("div");
+          box.classList.add("box");
+          box.style.width = door.clientWidth + "px";
+          box.style.height = door.clientHeight + "px";
+          box.textContent = pool[i];
+          boxesClone.appendChild(box);
+        }
+        boxesClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
+        boxesClone.style.transform = `translateY(-${
+          door.clientHeight * (pool.length - 1)
+        }px)`;
+        door.replaceChild(boxesClone, boxes);
+      }
+    }
+  
+    function shuffle([...arr]) {
+      let m = arr.length;
+      while (m) {
+        const i = Math.floor(Math.random() * m--);
+        [arr[m], arr[i]] = [arr[i], arr[m]];
+      }
+      return arr;
+    }
+  
+    init();
+  })();
+  
